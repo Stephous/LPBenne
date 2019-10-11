24ea1bf549639d4e9e77e6cf5f650a00ec2b3648
Takijix <tittouan.hinckel@gmail.com>
2019-10-11 10:39:11 +0200

    plop

--- MyBenne/app/src/main/java/com/example/mybenne/MainActivity.java
+++ MyBenne/app/src/main/java/com/example/mybenne/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 String WAZAAA;
+String plop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
