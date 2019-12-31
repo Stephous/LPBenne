8a7e775700533e4ad3f599621556767fff50090a
Stephous <stephane.dacheux@etud.u-picardie.fr>
2019-12-31 18:00:16 +0100

    FINI ENVOI SMS ET GEOLOCALISATION

--- MyBenne/app/src/main/java/com/example/mybenne/DatabaseManager.java
+++ MyBenne/app/src/main/java/com/example/mybenne/DatabaseManager.java
@@ -7,8 +7,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
-public class DatabaseManager extends SQLiteOpenHelper {
+import java.util.ArrayList;
 
+public class DatabaseManager extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "Benne.db";
 
     public static final String USER_KEY = "id";
@@ -23,9 +24,21 @@ public class DatabaseManager extends SQLiteOpenHelper {
             "CREATE TABLE " + TABLE_NAME + " (" +
                     USER_KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                     USER_NOM + " TEXT, " +
-                    USER_PRENOM + "TEXT, " +
-                    USER_TEL + "TEXT, " +
-                    USER_FONCTION + "TEXT);";
+                    USER_PRENOM + " TEXT, " +
+                    USER_TEL + " TEXT, " +
+                    USER_FONCTION + " TEXT);";
+
+    public static final String BENNE_KEY = "id";
+    public static final String BENNE_QR_CODE = "qrcode";
+    public static final String BENNE_POSITION = "position";
+
+    public static final String TABLE_BENNE = "benne";
+
+    public static final String TABLE_CREATE_BENNE =
+            "CREATE TABLE " + TABLE_BENNE + " (" +
+                    BENNE_KEY + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    BENNE_QR_CODE + " VARCHAR, " +
+                    BENNE_POSITION + " VARCHAR);";
 
     public DatabaseManager(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -34,14 +47,16 @@ public class DatabaseManager extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(TABLE_CREATE);
+        db.execSQL(TABLE_CREATE_BENNE);
     }
 
     public static final String TABLE_DROP = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
-
+    public static final String TABLE_DROP_BENNE = "DROP TABLE IF EXISTS " + TABLE_BENNE + ";";
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL(TABLE_DROP);
+        db.execSQL(TABLE_DROP_BENNE);
         onCreate(db);
     }
 
@@ -60,10 +75,36 @@ public class DatabaseManager extends SQLiteOpenHelper {
             return true;
     }
 
-    public Cursor getAllData(String fonction_recu) {
+    public boolean insertDataBenne(String id_benne,String qr_code, String position)
+    {
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor result=db.rawQuery("SELECT nom, prenom, telephone FROM " + TABLE_NAME + " WHERE " + USER_FONCTION + "=" + fonction_recu + " ORDER BY " + USER_KEY + " DESC LIMIT 1;", null);
-        return result;
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(BENNE_KEY, id_benne);
+        contentValues.put(BENNE_QR_CODE, qr_code);
+        contentValues.put(BENNE_POSITION, position);
+        long result= (db.insert(TABLE_BENNE, null, contentValues));
+        if(result== -1)
+            return false;
+        else
+            return true;
     }
 
+    public Cursor getDataNom(String fonction_recu) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor resultNom=db.rawQuery("SELECT nom, prenom, telephone FROM " + TABLE_NAME + " WHERE " + USER_FONCTION + "=" + "\'" + fonction_recu + "\'" + " ORDER BY " + USER_KEY + " DESC LIMIT 1;", null);
+        return resultNom;
+    }
+    public String[] getDataBenne(String fonction_recu) {
+
+        ArrayList<String> columnArray1 = new ArrayList<>();
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        Cursor resultBenne=db.rawQuery("SELECT position FROM " + TABLE_BENNE + " WHERE " + BENNE_KEY + "=" + fonction_recu , null);
+
+        columnArray1.add(resultBenne.getString(1));
+
+        String[] colStrArr1 = columnArray1.toArray(new String[columnArray1.size()]);
+        return colStrArr1;
+    }
 }
--- MyBenne/app/src/main/java/com/example/mybenne/Geolocalisation.java
+++ MyBenne/app/src/main/java/com/example/mybenne/Geolocalisation.java
@@ -1,7 +1,24 @@
 package com.example.mybenne;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.DatabaseUtils;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,35 +27,127 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class Geolocalisation extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Button BetaMairie;
+    DatabaseManager db = new DatabaseManager(this);
+    SharedPreferences permissionStatus;
+    private boolean sentToSettings = false;
+    private static final int SMS_PERMISSION_CONSTANT = 100;
+    private static final int REQUEST_PERMISSION_SETTING = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geolocalisation);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        BetaMairie = findViewById(R.id.BetamoveBenne);
+        BetaMairie.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                /**
+                 *  NUM DESTINATAIRE
+                 */
+                Cursor colStrArr1 = db.getDataNom("destinataire");
+                colStrArr1.moveToFirst();
+                Log.v("Cursor Object", DatabaseUtils.dumpCursorToString(colStrArr1));
+                String destnumber = colStrArr1.getString(2);
+                Toast.makeText(Geolocalisation.this, destnumber , Toast.LENGTH_LONG).show();
+
+
+                /**
+                 *  INFO BENNE
+                 */
+                String idBenne = "1";
+                String posbenne = "49.847177, 3.287543";
+
+                /**
+                 *  INFO UTILISATEUR
+                 */
+                Cursor colStrArr2 = db.getDataNom("utilisateur");
+                colStrArr1.moveToFirst();
+                Log.v("Cursor Object", DatabaseUtils.dumpCursorToString(colStrArr1));
+                String userinfo = "utilisateur :" +
+                        colStrArr1.getString(0) +
+                        colStrArr1.getString(1) +
+                        colStrArr1.getString(2);
+
+
+
+                String message = "Benne n°" + idBenne + " Aux coordonnées " + posbenne + "mise à jour par : " + userinfo;
+                SendMessage(destnumber, message);
+
+
+                if (ActivityCompat.checkSelfPermission(Geolocalisation.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(Geolocalisation.this, Manifest.permission.SEND_SMS)) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(Geolocalisation.this);
+                        builder.setTitle("Need SMS Permission");
+                        builder.setMessage("This app needs SMS permission to send Messages.");
+                        builder.setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                                ActivityCompat.requestPermissions(Geolocalisation.this,
+                                        new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION_CONSTANT);
+                            }
+                        });
+                        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                        builder.show();
+                    } else {
+                        ActivityCompat.requestPermissions(Geolocalisation.this, new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION_CONSTANT);
+                    }
+
+//                    SharedPreferences.Editor editor = permissionStatus.edit();
+//                    editor.putBoolean(Manifest.permission.SEND_SMS, true);
+//                    editor.commit();
+
+                }
+                /**
+                 * SMS
+                 */
+
+//                SmsManager.getDefault().sendTextMessage(destnumber,null,
+//                        "Benne n°" + idBenne +
+//                        " Aux coordonnées " + posbenne +
+//                        "mise à jour par : " + userinfo,null,null);
+            }
+        });
     }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+//    boolean isInserted=db.insertDataBenne(BENNE_QR_CODE.getText().toString(), BENNE_POSITION.getText().toString());
+//    boolean B1=db.insertDataBenne("1","", "49.847177, 3.287543");
+//    boolean B2=db.insertDataBenne("2","", "49.846054, 3.286342");
+//    boolean B3=db.insertDataBenne("3","", "49.847342, 3.287045");
+//    boolean B4=db.insertDataBenne("4","", "49.845640, 3.290009");
+//    boolean B5=db.insertDataBenne("5","", "49.845280, 3.284628");
+    public void SendMessage(String strMobileNo, String strMessage) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(strMobileNo, null, strMessage, null, null);
+            Toast.makeText(getApplicationContext(), "Your Message Sent",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(), ex.getMessage().toString(), Toast.LENGTH_LONG).show();
+            Toast.makeText(Geolocalisation.this, "if you agreed permissions , repress the send button" , Toast.LENGTH_LONG).show();
+        }
+    }
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
         LatLng BenneMairie = new LatLng(49.847177, 3.287543);
+//        LatLng BenneMairie = new LatLng(Double.parseDouble(db.getDataBenne("1")[0]),Double.parseDouble(db.getDataBenne("1")[1]));
         LatLng BenneHotelMemorial = new LatLng(49.846054, 3.286342);
         LatLng BenneGoldenPub = new LatLng(49.847342, 3.287045);
         LatLng BenneBoulanderiePaul = new LatLng(49.845640, 3.290009);
@@ -51,3 +160,6 @@ public class Geolocalisation extends FragmentActivity implements OnMapReadyCallb
         mMap.moveCamera(CameraUpdateFactory.newLatLng(BenneMairie));
     }
 }
+
+
+
