01c4d573785e889b46e6878f0159e79db1a63e5e
Stephous <stephane.dacheux@etud.u-picardie.fr>
2020-01-01 21:16:57 +0100

    Add buggé du qr code

--- MyBenne/app/src/main/java/com/example/mybenne/QrCode.java
+++ MyBenne/app/src/main/java/com/example/mybenne/QrCode.java
@@ -2,12 +2,40 @@ package com.example.mybenne;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import com.example.mybenne.R;
 
-public class QrCode extends AppCompatActivity {
+public class QrCode extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnTakePicture, btnScanBarcode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_code);
+        initViews();
+    }
+
+    private void initViews() {
+        btnTakePicture = findViewById(R.id.btnTakePicture);
+        btnScanBarcode = findViewById(R.id.btnScanBarcode);
+        btnTakePicture.setOnClickListener(this);
+        btnScanBarcode.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()) {
+            case R.id.btnTakePicture:
+                startActivity(new Intent(QrCode.this, PictureBarcodeActivity.class));
+                break;
+            case R.id.btnScanBarcode:
+                startActivity(new Intent(QrCode.this, ScannedBarcodeActivity.class));
+                break;
+        }
+
     }
 }
